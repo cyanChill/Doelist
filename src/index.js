fa837ed6750a76468777b112c8a27552314f3402
cyanchill <anthonyliang9@gmail.com>
2021-08-30 14:39:42 -0400

    general transitions with navbar pretty much completed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 const navToggler = document.getElementById("nav-toggle");
 const navTogglerLabel = document.getElementById("nav-toggle-label");
 const navBar = document.getElementById("side-nav-bar");
+const header = document.getElementById("header");
+const taskHeader = document.getElementById("task-header");
+const taskList = document.getElementById("task-list");
 
 navToggler.addEventListener("change", (e) => {
-  navBar.classList.toggle("active");
-  if (e.target.checked) {
-    navTogglerLabel.innerHTML = '<i class="fas fa-times icon"></i>';
-    fixDOMonNav();
-  } else {
-    navTogglerLabel.innerHTML = '<i class="fas fa-bars icon"></i>';
-    unFixDOMonNav();
-  }
+  changeNavVis();
 });
 
-function fixDOMonNav() {
-  document.body.style.top = `-${window.scrollY}px`;
-  document.body.style.position = "fixed";
+function changeNavVis() {
+  if (navToggler.checked) {
+    navTogglerLabel.innerHTML = '<i class="fas fa-times icon"></i>';
+  } else {
+    navTogglerLabel.innerHTML = '<i class="fas fa-bars icon"></i>';
+  }
+  navBar.classList.toggle("active");
 }
 
-function unFixDOMonNav() {
-  const scrollY = document.body.style.top;
-  document.body.style.position = "";
-  document.body.style.top = "";
-  window.scrollTo(0, parseInt(scrollY || "0") * -1);
+function todosListHeight() {
+  const offsetHeight = header.offsetHeight + taskHeader.offsetHeight;
+  taskList.style.height = `max(3rem, calc(100vh - ${offsetHeight}px - 3rem))`;
 }
+
+let prevWidth = window.innerWidth;
+
+window.addEventListener("resize", () => {
+  todosListHeight();
+  if (window.innerWidth > 725 && window.innerWidth < 775) {
+    if (
+      (window.innerWidth <= 750 && prevWidth > window.innerWidth && navToggler.checked) ||
+      (window.innerWidth >= 750 && prevWidth < window.innerWidth && !navToggler.checked)
+    ) {
+      navToggler.checked = !navToggler.checked;
+      changeNavVis();
+    }
+  }
+  prevWidth = window.innerWidth;
+});
+
+todosListHeight();
