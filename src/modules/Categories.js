ebbafd8c96f062c049321808e73d536b4dc8f1a1
cyanchill <anthonyliang9@gmail.com>
2021-08-31 21:32:13 -0400

    starting creation of modules

--- src/modules/Categories.js
+++ src/modules/Categories.js
@@ -2,7 +2,7 @@ import { createIcon } from "./utility";
 import { createTaskCard } from "./Tasks";
 import { closeNavOnMobile } from "./NavBar";
 
-const taskHeader = document.getElementById("task-header");
+const taskCategoryHeader = document.getElementById("task-category-header");
 const taskList = document.getElementById("task-list");
 
 const CategoryIcons = {
@@ -18,23 +18,23 @@ function displayCategory(categoryName, tasksList) {
   taskList.textContent = "";
 
   const categoryTasks = tasksList.filter((task) => {
-    return task.project === categoryName;
+    return task.categoryLocation === categoryName;
   });
 
   if (categoryName !== "Completed") {
     categoryTasks.forEach((task) => {
-      taskList.appendChild(createTaskCard(task, { completed: false }));
+      taskList.appendChild(createTaskCard(task));
     });
   } else {
     categoryTasks.forEach((task) => {
-      taskList.appendChild(createTaskCard(task, { completed: true }));
+      taskList.appendChild(createTaskCard(task));
     });
   }
 }
 
 function updateTaskHeader(categoryName) {
-  taskHeader.textContent = "";
-  taskHeader.appendChild(createCategoryLabel(categoryName));
+  taskCategoryHeader.textContent = "";
+  taskCategoryHeader.appendChild(createCategoryLabel(categoryName));
 }
 
 function createCategoryLabel(categoryName) {
@@ -76,5 +76,36 @@ function createCategory(categoryName) {
   });
   return categoryDiv;
 }
+
+const Categories = (function () {
+  const categoryList = JSON.parse(localStorage.getItem("categoryList")) || [
+    "Inbox",
+    "Today",
+    "Upcoming",
+    "Anytime",
+    "Completed",
+  ];
+
+  function addCategory(categoryName) {
+    console.log("category added");
+    updateLocalStorage();
+  }
+
+  function deleteCategory(categoryName) {
+    console.log("category added");
+    updateLocalStorage();
+  }
+
+  function updateLocalStorage() {
+    localStorage.setItem("categoryList", JSON.stringify(categpryList));
+  }
+
+  return { categoryList, addCategory, deleteCategory };
+})();
+
+const CategoryForm = (function () {
+  
+  console.log("");
+})();
 
 export { displayCategory, setSelected, createCategory };
--- src/modules/TasksList.js
+++ src/modules/TasksList.js
@@ -1,26 +1,63 @@
-const header = document.getElementById("header");
-const taskHeader = document.getElementById("task-header");
-const taskListDiv = document.getElementById("task-list");
+import { createTaskCard } from "./Tasks";
 
-function taskListHeight() {
-  const offsetHeight = header.offsetHeight + taskHeader.offsetHeight;
-  taskListDiv.style.height = `max(3rem, calc(100vh - ${offsetHeight}px - 3rem))`;
-}
+const TaskList = (function () {
+  const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
 
-/* 
-  Make a "TaskList" object to keep track of all the tasks - update local storage when we add/update/remove an Task
-*/
+  function addTask(task) {
+    taskList.push(task);
+    updateLocalStorage();
 
-const TaskList = () => {
-  let taskList = [];
+    /* Only add task to DOM if the task belongs to the current category displayed on the DOM - can figure it out by finding the current DOM element with the "selected" class */
+    TaskListDOM.addToList(task);
+  }
 
-  const addTask = () => {};
+  function updateTask(origTask, newTask) {
+    const taskIdx = taskList.indexOf(origTask);
+    if (taskIdx === -1) return;
+    taskList.splice(taskIdx, 1, newTask);
+    updateLocalStorage();
 
-  const updateTask = () => {};
+    /* Function to update page if updated item belongs to current page category - search dom for that element and update it from page instead of reloading contents */
+  }
 
-  const removeTask = () => {};
+  function removeTask(task) {
+    const taskIdx = taskList.indexOf(task);
+    if (taskIdx === -1) return;
+    taskList.splice(taskIdx, 1);
+    updateLocalStorage();
+
+    /* Function to update page if deleted item belongs to current page category - search dom for that element and delete it from page instead of reloading contents */
+  }
+
+  function updateLocalStorage() {
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+  }
 
   return { taskList, addTask, updateTask, removeTask };
-};
+})();
 
-export { taskListHeight };
+const TaskListDOM = (function () {
+  const header = document.getElementById("header");
+  const taskCategoryHeader = document.getElementById("task-category-header");
+  const taskListDiv = document.getElementById("task-list");
+
+  function taskListHeight() {
+    const offsetHeight = header.offsetHeight + taskCategoryHeader.offsetHeight;
+    taskListDiv.style.height = `max(3rem, calc(100vh - ${offsetHeight}px - 3rem))`;
+  }
+
+  function addToList(task) {
+    const newTask = createTaskCard(task);
+    taskListDiv.appendChild(newTask);
+  }
+
+  function removeFromList() {
+    console.log("removing item");
+    /* Do it like how we did it with the library app - when we click the edit button which, it will log the current element as a global variable
+    (it'll find the parent element [the task card] and save it in an object for example) */
+  }
+
+  return { taskListHeight, addToList };
+})();
+
+export { TaskList, TaskListDOM };
