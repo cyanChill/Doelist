18a993f1877f77c7aa1cdc17a5d051970ab23384
cyanchill <anthonyliang9@gmail.com>
2021-09-02 20:12:17 -0400

    add functionality to add a new category or a new task

--- src/modules/Forms.js
+++ src/modules/Forms.js
@@ -1,9 +1,15 @@
+import { Categories } from "./Categories";
+import { TaskList } from "./TasksList";
+import { Task } from "./Tasks";
+
 const Forms = (function () {
   const overlayBKG = document.querySelector(".overlay-bkg");
   const addCategoryFormDiv = document.getElementById("add-category");
   const addCategoryFormElement = addCategoryFormDiv.querySelector("form");
+  const categoryErrorField = addCategoryFormDiv.querySelector(".errorField");
   const taskFormDiv = document.getElementById("add-edit-task");
   const taskFormElement = taskFormDiv.querySelector("form");
+  const taskErrorField = taskFormDiv.querySelector(".errorField");
   const exitFormButtons = document.querySelectorAll(".overlay-form span.close-form");
   const newCategoryBtn = document.getElementById("new-category");
   const newTaskBtn = document.getElementById("add-task");
@@ -12,19 +18,32 @@ const Forms = (function () {
     btn.addEventListener("click", hideForms);
   });
 
-  newCategoryBtn.addEventListener("click", showCategoryForm);
-  newTaskBtn.addEventListener("click", showTaskForm);
+  newCategoryBtn.addEventListener("click", () => {
+    displayForm("categoryForm");
+  });
+  newTaskBtn.addEventListener("click", () => {
+    displayForm("add-editForm");
+  });
 
-  function showCategoryForm() {
-    formScreenEnter();
-    addCategoryFormDiv.classList.remove("hidden");
-    // Listen to if we click outside the form (to close "add form" screen)
-    document.addEventListener("click", exitForm);
+  document.addEventListener("submit", handleSubmit);
+
+  function displayFormError(msg) {
+    categoryErrorField.textContent = msg;
+    taskErrorField.textContent = msg;
   }
 
-  function showTaskForm() {
+  function clearFormError() {
+    categoryErrorField.textContent = "";
+    taskErrorField.textContent = "";
+  }
+
+  function displayForm(formName) {
     formScreenEnter();
-    taskFormDiv.classList.remove("hidden");
+    if (formName === "categoryForm") {
+      addCategoryFormDiv.classList.remove("hidden");
+    } else if (formName === "add-editForm") {
+      taskFormDiv.classList.remove("hidden");
+    }
     // Listen to if we click outside the form (to close "add form" screen)
     document.addEventListener("click", exitForm);
   }
@@ -46,12 +65,39 @@ const Forms = (function () {
 
     taskFormDiv.classList.add("hidden");
     taskFormElement.reset();
+    clearFormError();
   }
 
   function exitForm(e) {
     if (e.target.dataset.hasOwnProperty("outside")) {
       hideForms();
     }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formName = e.target.dataset.form;
+
+    if (formName === "add-category") {
+      const categoryInVal = e.target["category-name"].value;
+
+      if (Categories.getCategoryListCopy().includes(categoryInVal)) {
+        displayFormError("Error: Task Category Already Exists");
+        return;
+      }
+
+      Categories.addCategory(categoryInVal);
+    } else if (formName === "add-edit-task") {
+      const newTask = Task(
+        e.target["task-name"].value,
+        e.target["task-description"].value,
+        e.target["priority-select"].value,
+        e.target["category-select"].value,
+        e.target["due-date"].value
+      );
+      TaskList.addTask(newTask);
+    }
+    hideForms();
   }
 
   return {};
